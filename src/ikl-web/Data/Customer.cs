f3058323ba16a6a7cacda33b0c211b6c99820eb0
Jonas Gottschau <jlg@brandarmies.com>
2020-11-23 23:14:51 +0100

    initial commit

---
+++ src/ikl-web/Data/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ikl_web.Data
+{
+    public class Customer
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("number")]
+        public int Number { get; set; }
+        [JsonPropertyName("year")]
+        public int Year { get; set; }
+        [JsonPropertyName("names")]
+        public string[] Names { get; set; }
+        
+    }
+}
---
+++ src/ikl-web/Data/DrawingService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ikl_web.Data
+{
+    public class DrawingService
+    {
+        private readonly ICollection<Drawing> _drawings;
+
+        public DrawingService(ICollection<Drawing> drawings)
+        {
+            _drawings = drawings;
+        }
+    }
+}
