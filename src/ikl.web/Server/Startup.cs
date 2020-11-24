818f4ede8d6d0c1aae3b29357a93e1011568026b
Jonas Gottschau <jlg@brandarmies.com>
2020-11-24 19:35:53 +0100

    change to use blazor wasm

---
+++ src/ikl.web/Server/Startup.cs
@@ -0,0 +1,71 @@
+using ikl.web.Shared;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ikl.web.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            var customersJson = File.ReadAllText("customers.json");
+            var customers = JsonSerializer.Deserialize<ICollection<Customer>>(customersJson);
+            foreach (var item in customers)
+            {
+                item.Year += 1900;
+            }
+            services.AddSingleton(sp => customers);
+            var drawingsJson = File.ReadAllText("drawings.json");
+            services.AddSingleton(sp => JsonSerializer.Deserialize<ICollection<Drawing>>(drawingsJson));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
---
+++ src/ikl.web/Shared/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ikl.web.Shared
+{
+    public class Customer
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("number")]
+        public int Number { get; set; }
+        [JsonPropertyName("year")]
+        public int Year { get; set; }
+        [JsonPropertyName("names")]
+        public string[] Names { get; set; }
+        
+    }
+}
