e3ecd3a61773f770083b8add3e3f7dae89b47685
Jonas Gottschau <jlg@brandarmies.com>
2021-01-18 08:25:15 +0100

    brug matblazor

--- src/ikl.web/Client/Program.cs
+++ src/ikl.web/Client/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using ikl.web.Client.Shared;
+using MatBlazor;
 
 namespace ikl.web.Client
 {
@@ -17,7 +18,8 @@ namespace ikl.web.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services.AddMatBlazor();
+            
             builder.Services.AddSingleton(sp => new HttpClient
                 {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
             builder.Services.AddSingleton<DataService>();
--- src/ikl.web/Client/Shared/DataService.cs
+++ src/ikl.web/Client/Shared/DataService.cs
@@ -12,7 +12,8 @@ namespace ikl.web.Client.Shared
         private readonly HttpClient _httpClient;
 
         private Data _data;
-        
+
+        public HashSet<string> Tags = new HashSet<string>();
         public DataService(HttpClient httpClient)
         {
             _httpClient = httpClient;
@@ -21,6 +22,13 @@ namespace ikl.web.Client.Shared
         public async Task Initialize()
         {
             _data = await _httpClient.GetFromJsonAsync<Data>("data");
+            foreach (var drawing in _data.Drawings)
+            {
+                foreach (var tag in drawing.Tags)
+                {
+                    Tags.Add(tag);
+                }
+            }
         }
 
         public Customer[] GetCustomers()
@@ -40,6 +48,14 @@ namespace ikl.web.Client.Shared
                 .Drawings
                 .Where(d => !(d.Ratios.Length == 1 && d.Ratios[0] == "1:1"))
                 .Where(d => d.CustomerId.Equals(customerId)).ToList();
+        }
+        
+        public List<Drawing> GetDrawings()
+        {
+            return _data
+                .Drawings
+                .Where(d => !(d.Ratios.Length == 1 && d.Ratios[0] == "1:1"))
+                .ToList();
         }
         
         public List<Drawing> SearchDrawings(string text)
